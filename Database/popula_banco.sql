1520d098bc5b45ba092c340b1b5045440ea1aff7
VhLucena <v-lucena@outlook.com>
2018-09-28 20:06:51 -0300

    popula_banco atualizado

--- Database/popula_banco.sql
+++ Database/popula_banco.sql
@@ -10,14 +10,14 @@ INSERT INTO clinica VALUES (123456789101114, 'Clinica 3', 'Joelson Muller', 'Rua
 INSERT INTO atendente VALUES (00000000000, 'Jamile Souza', 'jami_souza@gmail.com', 'Rua da Jamile, 123', '5300000000', 'admin');
 
 #Insere Medicos
-INSERT INTO medico VALUES (1111, 12300012301, 'Robervildson Silveira', '1973-12-06', 'robervilds@gmail.com', 'Rua do Rober, 123', '53987665781', 'Odontologista', 'robervilds123');
-INSERT INTO medico VALUES (2222, 12300012302, 'Adilson Moreira', '1975-10-09', 'ad_moreira@gmail.com', 'Rua do Adilson, 123', '53987665782', 'Cardiologista', 'adilsao000');
-INSERT INTO medico VALUES (3333, 12300012303, 'Maria da Silva', '1987-06-15', 'msilva@gmail.com', 'Rua da Maria, 123', '53987665783', 'Pediatra', 'maria123');
+INSERT INTO medico VALUES (1111, 12300012301, 'Robervildson Silveira', '1973-12-06', 'robervilds@gmail.com', 'Rua do Rober, 123', '53987665781', 'Odontologista', 'med', 'robervilds123');
+INSERT INTO medico VALUES (2222, 12300012302, 'Adilson Moreira', '1975-10-09', 'ad_moreira@gmail.com', 'Rua do Adilson, 123', '53987665782', 'Cardiologista', 'med', 'adilsao000');
+INSERT INTO medico VALUES (3333, 12300012303, 'Maria da Silva', '1987-06-15', 'msilva@gmail.com', 'Rua da Maria, 123', '53987665783', 'Pediatra', 'med', 'maria123');
 
 #Insere Pacientes
-INSERT INTO paciente VALUES (11111111111, 'Paciente 1', '1991-01-01', 'paciente1@gmail.com', 'Rua do Paciente 1, 111', '5311111111', 'paciente1');
-INSERT INTO paciente VALUES (22222222222, 'Paciente 2', '1992-02-02', 'paciente2@gmail.com', 'Rua do Paciente 2, 222', '5322222222', 'paciente2');
-INSERT INTO paciente VALUES (33333333333, 'Paciente 3', '1993-03-03', 'paciente3@gmail.com', 'Rua do Paciente 3, 333', '5333333333', 'paciente3');
+INSERT INTO paciente VALUES (11111111111, 'Paciente 1', '1991-01-01', 'paciente1@gmail.com', 'Rua do Paciente 1, 111', '5311111111', 'pac', 'paciente1');
+INSERT INTO paciente VALUES (22222222222, 'Paciente 2', '1992-02-02', 'paciente2@gmail.com', 'Rua do Paciente 2, 222', '5322222222', 'pac', 'paciente2');
+INSERT INTO paciente VALUES (33333333333, 'Paciente 3', '1993-03-03', 'paciente3@gmail.com', 'Rua do Paciente 3, 333', '5333333333', 'pac', 'paciente3');
 
 #Insere Consultas
 INSERT INTO consulta VALUES (1111, 22222222222, '10:00:00', '2018-09-21', 123456789101112, NULL, NULL);
